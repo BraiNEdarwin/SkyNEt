8171121648b836327927562bfca17248914ce324
William Elferink <william.elferink@gmail.com>
2018-03-01 15:49:10 +0100

    Dummy files added
    Dummy files added to empty folder to transfer structure

--- SkyNEt.py
+++ SkyNEt.py
@@ -1,3 +1,12 @@
 ''''
 Main handler of the SkyNEt platform
 '''
+
+import modules.ReservoirSparse as Reservoir
+
+nodes = [1, 50, 1]  # [input, reservoir, output]
+inputscaling = 0.2
+spectralradius = 0.99
+weightdensity = 1
+
+res = Reservoir.Network(nodes, inputscaling, spectralradius, weightdensity)
