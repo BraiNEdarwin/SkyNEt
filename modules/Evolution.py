65095d42a1a5227e7921f3284ce7c1856596d59c
brambozz <brambo5@live.nl>
2018-03-02 16:39:22 +0100

    Evolution module
    A small beginning with the evolution module. Still some stuff to be done.

--- modules/Evolution.py
+++ modules/Evolution.py
@@ -1,3 +1,19 @@
 '''
 Handles evolution using fitness from post-process
 '''
+import numpy as np
+
+class GenePool(object):
+
+	def __init__(self, genes, genomes, generange):
+
+		self.pool = np.empty(genes + 1, genomes)  #last row is used for fitness score
+
+		for i in range(genes):
+			for j in range(genomes):
+				self.pool[i,j] = generange[i][0] + (generange[i][1] - generange[i][0]) * np.random.rand()
+
+	def nextGen(self):
+		indices = range(genes + 1)
+		indices.sort()
+		fitnessArray = self.pool[genes]
