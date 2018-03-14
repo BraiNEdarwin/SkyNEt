ee728b087cfe3ed060286ba55f9d92884210bb58
brambozz <brambo5@live.nl>
2018-03-14 17:01:51 +0100

    minor file replacement

--- ivvi_test.py
+++ ivvi_test.py
@@ -3,13 +3,13 @@ quick test script to test connection to the IVVI module.
 '''
 import time
 import matplotlib.pyplot as plt
-import qcodes
+#import qcodes
 #from qcodes.instrument_drivers.stanford_research.SR860 import SR860
 from qcodes.instrument_drivers.QuTech.IVVI import IVVI
 
-ivvi = IVVI("ivvi", "COM5")
+#ivvi = IVVI("ivvi", "COM5")
 
-ivvi.set_dacs_zero()
+#ivvi.set_dacs_zero()
 
 # test setting the first dac to 50 mV value
 # ivvi.dac1(50)
