01447526e6424bb0003b9e0c62342dc006a50966
brambozz <brambo5@live.nl>
2018-04-10 14:58:01 +0200

    new fitness function
    Implemented a new fitness function for evolution. This is a combination of (i) the traditional fitness functino, (ii) a shape-only part, (iii) the fitness quality parameter. Todo: implement a novelty component.

--- modules/PostProcess.py
+++ modules/PostProcess.py
@@ -28,23 +28,43 @@ def trainNetwork(method, output, target, a):
 def fitness(x, target):
     return 1 / ((np.linalg.norm(x - target, 2)) ** 2 * (1 / len(x)))
 
-def fitnessEvolution(x, target, W):
-    
+def fitnessEvolution(x, target, W, par):
+    #this implements the fitness function
+    #F = par[0] * m / (sqrt(r) + par[3] * abs(c)) + par[1] / r + par[2] * Q
+    #where m,c,r follow from fitting x = m*target + c to minimize r
+    #and Q is the fitness quality as defined by Celestine in his thesis
+    #appendix 9
+
+
     #extract fit data with weights W
     indices = np.argwhere(W)  #indices where W is nonzero (i.e. 1)
-    indices = indices[0]  #extract np array
 
     x_weighed = np.empty(len(indices))
     target_weighed = np.empty(len(indices))
     for i in range(len(indices)):
-    	x_weighed = x[i]
-    	target_weighed = target[i]
+    	x_weighed[i] = x[indices[i]]
+    	target_weighed[i] = target[indices[i]]
 
 
 	#fit x = m * target + c to minimize res
-    A = np.vstack([target, np.ones(len(indices))]).T  #write x = m*target + c as x = A*target 
-    m, c = np.linalg.lstsq(A, x_weighed)[0]	
-    res = np.linalg.lstsq(A, x_weighed)[1]
-    
-    return target_weighed
+    A = np.vstack([target_weighed, np.ones(len(indices))]).T  #write x = m*target + c as x = A*(m, c)
+    m, c = np.linalg.lstsq(A, x_weighed, rcond=None)[0]	
+    res = np.linalg.lstsq(A, x_weighed, rcond=None)[1]
+    res = res[0]
+
     #determine fitness quality
+    indices1 = np.argwhere(target_weighed)  #all indices where target is nonzero
+    x0 = np.empty(0)  #list of values where x should be 0
+    x1 = np.empty(0)  #list of values where x should be 1
+    for i in range(len(target_weighed)):
+        if(i in indices1):
+            x1 = np.append(x1, x_weighed[i])
+        else:
+            x0 = np.append(x0, x_weighed[i])
+    if(min(x1) < max(x0)):
+        Q = 0
+    else:
+        Q = (min(x1) - max(x0)) / (max(x1) - min(x0) + abs(min(x0)))
+    F = par[0] * m / (res**(.5) + par[3] * abs(c)) + par[1] / res + par[2] * Q
+    return F, m, c
+
--- postprocess_test.py
+++ postprocess_test.py
@@ -5,8 +5,23 @@ import matplotlib.pyplot as plt
 
 
 t, x, y, W = GenerateInput.softwareInput(['bl','AND'], 1000, 5, 10)
+par = [1, 1, 1, 0.1]
+test_data = np.empty(530)
+test_data[0:125] = 0
+test_data[125:135] = -1
+test_data[135:260] = 0.5
+test_data[260:270] = -1
+test_data[270:395] = 0
+test_data[395:405] = -1
+test_data[405:530] = 1
+noise = np.random.rand(530) * 0.1
+test_data = test_data + noise
+F, m, c = PostProcess.fitnessEvolution(test_data, x, W, par)
 
-target_weighed = PostProcess.fitnessEvolution(x, y, W)
-
-plt.plot(target_weighed)
+plt.plot(x)
+plt.plot((test_data - c) / m)
+#plt.plot(min0 * np.ones(530),'r--')
+#plt.plot(max0 * np.ones(530),'r--')
+#plt.plot(min1 * np.ones(530),'b--')
+#plt.plot(max1 * np.ones(530),'b--')
 plt.show()
