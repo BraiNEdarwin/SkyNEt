8c7bb969efaa143319bdba58dc516606d4ac730e
Bvdv <37548649+bvdv@users.noreply.github.com>
2018-05-02 15:37:45 +0200

    dac start

--- modules/benchmarks/DACevo.py
+++ modules/benchmarks/DACevo.py
@@ -5,4 +5,15 @@ import time
 import numpy as np
 
 
-def DacMeasure(x, Fs)
+x = np.matrix('1, 1, 1, 0, 0, 0; 1, 0, 0, 1, 1, 0; 0, 1, 0, 1, 0, 1; 0, 0, 1, 0, 1, 1')
+inputvoltages = np.zeros(len(x))
+
+def DacMeasure(x, Fs):
+	for i in range(np.shape(x)[1]):
+		for j in range(len(x)):
+			inputvoltages[j] = x[j,i]
+	
+		IVVIrack.setinputVoltages(ivvi, inputVoltages)
+		time.sleep(1)
+
+	#measure and save to first part of voltage and time data.
