8c6098596c39844bac99183721fc5f09cdc8d6fc
Bvdv <37548649+bvdv@users.noreply.github.com>
2018-05-07 11:46:03 +0200

    keitleyadded

--- modules/benchmarks/DACevo.py
+++ modules/benchmarks/DACevo.py
@@ -6,7 +6,7 @@ from instruments.niDAQ import nidaqIO
 from instruments.DAC import IVVIrack
 
 
-def DacMeasure(x, target, Fs):
+def DacMeasuremani(x, target, Fs):
 	inputvoltages = np.zeros(len(x))
 	singlemeas = target[0:len(target)/np.shape(x)[1]]
 	output = np.zeros(len(target))
@@ -27,6 +27,31 @@ def DacMeasure(x, target, Fs):
 
 
 
+def DacMeasuredigit(x, target, Fs):
+	x1 = np.matrix('0 0 0 0 0 1 1 0 1 1 1 0 0 0 1 0 0 0 0 1 0 1 1 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0; 0 1 1 1 0 1 1 0 1 1 1 1 1 1 0 1 1 1 1 0 0 1 1 0 1 0 1 1 1 1 0 1 1 1 1 1 1 1 1 0 0 1 1 1 0 0 1 1 1 0 ; 0 1 1 1 0 1 1 0 1 1 1 1 1 0 1 1 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 0 1 0 0 0 0 0 0 0 0 0 0; 0 1 1 1 0 1 1 0 1 1 1 1 0 1 1 1 1 1 1 0 1 1 1 0 1 1 1 1 1 0 0 1 1 1 0 1 1 1 0 1 0 1 1 1 0 1 1 1 1 0; 0 0 0 0 0 1 1 0 1 1 1 0 0 0 0 0 0 0 0 1 1 1 1 0 1 0 0 0 0 0 0 0 0 0 0 1 1 1 0 1 0 0 0 0 0 1 1 1 1 0')
 
+	print(np.shape(x1)[1])
 
-	#measure and save to first part of voltage and time data.
+	for n in range(10):
+		print(n)
+		x = x1[:,n*5:n*5+5]
+		for i in range(len(x)):
+			inputvoltages = x[i,:]
+			print(inputvoltages)
+			# IVVIrack.setinputVoltages(ivvi, inputVoltages)
+			# time.sleep(1)
+
+			# measure using desire module.
+
+			# output[i, n] = measureddata fill output in in array
+
+		for j in range(len(x)):
+			inputvoltages = x[:,j].T
+			print(inputvoltages)
+			# IVVIrack.setinputVoltages(ivvi, inputVoltages)
+			# time.sleep(1)
+	
+			# # measure using desire module.
+
+			# output[i+j+1, n] = measureddata 
+
