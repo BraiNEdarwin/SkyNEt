3f53bc36defd3c8190d1492186d423c1f7d9b093
hcruiz <hans.ruiz.euler@hotmail.com>
2018-08-14 16:54:25 +0200

    first commit working on GA; work in progress. TODO: encapsulate in crossover methods and vectorize. Define a mixingrate for the crossover

--- modules/Evolution.py
+++ modules/Evolution.py
@@ -4,94 +4,83 @@ Handles evolution using fitness from post-process
 import numpy as np
 
 
-def mapGenes(generange, gene):
-    return generange[0] + gene * (generange[1] - generange[0])
-
-
 class GenePool(object):
 
-    def __init__(self, genes, genomes):
+    def __init__(self, config_obj):
 
-        self.genes = genes
-        self.genomes = genomes
-        self.pool = np.random.rand(genes, genomes)
-        self.fitness = np.empty(genomes)
+        self.genes = len(config_obj.generange)
+        self.genomes = config_obj.genomes
+        self.pool = np.random.rand(self.genomes, self.genes)
+        self.fitness = np.zeros(self.genomes)
+        self.partition = config_obj.partition
+        self.mutationrate = config_obj.mutationrate
 
     def nextGen(self):
         indices = np.argsort(self.fitness)
         indices = indices[::-1]
-        fifth = int(self.genomes / 5)
-        newPool = self.pool
-        # promote fittest fifth of the pool
+        self.pool = self.pool[indices]
+        self.newpool = self.pool.copy() # promote fittest partition gene configurations
+
+        # generate second partition by adding Gaussian noise to the fittest partition
+        self.AddNoise()
+        # mutation
+        self.newpool[self.partition[0]:self.partition[1]] = self.mutation(
+                    self.newpool[self.partition[0]:self.partition[1]])
+         
+        ##TODO: encapsulate in crossover methods 
+        # generate third partition by mixing fittest n with fittest n+1
         for i in range(fifth):
-            newPool[:, i] = self.pool[:, indices[i]]
-
-        # generate second fifth
-        for i in range(fifth):
-            parent1 = newPool[:, i] * 1.05
-            parent2 = newPool[:, i] * 0.95
-
-            # check that genes are in [0,1]
             for j in range(self.genes):
-                if(parent1[j] > 1):
-                    parent1[j] = 1
-                if(parent1[j] < 0):
-                    parent1[j] = 0
-                if(parent2[j] > 1):
-                    parent2[j] = 1
-                if(parent2[j] < 0):
-                    parent2[j] = 0
-
-            for j in range(self.genes):
-                # crossover
                 if(np.random.rand() < 0.5):
-                    newPool[j, i + fifth] = parent1[j]
+                    newpool[j, i + 2 * fifth] = self.pool[j, indices[i]]
                 else:
-                    newPool[j, i + fifth] = parent2[j]
+                    newpool[j, i + 2 * fifth] = self.pool[j, indices[i + 1]]
 
                 # mutation
-                if(np.random.rand() < 0.1):
-                    newPool[j, i + fifth] = self.mutation(newPool[j, i + fifth])
+                newpool[j, i + 2 * fifth] = self.mutation(newpool[j, i + 2 * fifth])
 
-        # generate third fifth
+        # generate fourth partition by mixing fittest with randomly selected 
         for i in range(fifth):
             for j in range(self.genes):
                 if(np.random.rand() < 0.5):
-                    newPool[j, i + 2 * fifth] = self.pool[j, indices[i]]
+                    newpool[j, i + 3 * fifth] = newpool[j, i]
                 else:
-                    newPool[j, i + 2 * fifth] = self.pool[j, indices[i + 1]]
-
-                # mutation
-                if(np.random.rand() < 0.1):
-                    newPool[j, i + 2 * fifth] = self.mutation(newPool[j, i + 2 * fifth])
-
-        # generate fourth fifth
-        for i in range(fifth):
-            for j in range(self.genes):
-                if(np.random.rand() < 0.5):
-                    newPool[j, i + 3 * fifth] = newPool[j, i]
-                else:
-                    newPool[j, i + 3 * fifth] = self.pool[j,
+                    newpool[j, i + 3 * fifth] = self.pool[j,
                                                             np.random.randint(self.genomes)]
 
                 # mutation
-                if(np.random.rand() < 0.1):
-                    newPool[j, i + 3 * fifth] = self.mutation(newPool[j, i + 3 * fifth])
+                  newpool[j, i + 3 * fifth] = self.mutation(newpool[j, i + 3 * fifth])
 
-        # generate fifth fifth
+        # generate fifth partition by uniform sampling
         for i in range(fifth):
             for j in range(self.genes):
-                newPool[j, i + 4 * fifth] = np.random.rand()
+                newpool[j, i + 4 * fifth] = np.random.rand()
 
         # replace pool
-        self.pool = newPool   
+        self.pool = newpool   
         
         #empty fitness
-        self.fitness = np.empty(self.genomes) 
+        self.fitness = np.empty(self.genomes)
 
-    def returnPool(self):
-        return self.pool
     
-    def mutation(self,mode):
-        mutant = np.random.triangular(0,mode,1)
+    def Mutation(self,mode):
+        if(np.random.rand() < self.mutationrate):
+            mutant = np.random.triangular(0,mode,1)
+        else:
+            mutant = mode
         return mutant
+    
+    def MapGenes(self,generange, gene):
+        return generange[0] + gene * (generange[1] - generange[0])
+    
+    def AddNoise(self):
+        self.newpool[self.partition[0]:self.partition[1]] += 0.02*np.random.randn(
+                self.partition[1],self.newpool.shape[1])
+
+        # check that genes are in [0,1]
+        buff = self.newpool[self.partition[0]:self.partition[1]] > 1.0
+        self.newpool[self.partition[0]:self.partition[1]][buff] = 1.0
+        
+        buff = self.newpool[self.partition[0]:self.partition[1]] < 0.0
+        self.newpool[self.partition[0]:self.partition[1]][buff] = 0.0
+
