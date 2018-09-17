62d8e06abc94c54c995109e14e05e21adfa5d218
Bram de Wilde <brambo5@live.nl>
2018-09-17 11:28:07 +0200

    File deletion

--- init.py
+++
@@ -1,8 +0,0 @@
-from instruments.DAC import IVVIrack
-
-# initialize instruments
-ivvi = IVVIrack.initInstrument()
-
-# set the DAC voltages
-for k in range(genes):
-	IVVIrack.setControlVoltages(ivvi, controlVoltages)
