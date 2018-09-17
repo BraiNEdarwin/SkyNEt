14915770316cb46f6c1f5e5c518b4f17f83ec99b
Bram de Wilde <brambo5@live.nl>
2018-09-17 11:21:20 +0200

    File restructure 1
    Moved around some files and folders, also deleted some old modules. Still have to do all files in the main directory.

--- experiments/boolean_logic/[config]boolean_logic.py
+++ experiments/boolean_logic/[config]boolean_logic.py
@@ -1,16 +1,15 @@
-
 import numpy as np
 from config_class import config_class
 
 class experiment_config(config_class):
     '''This is a template for the configuration class that is experiment/user specific.
     It inherits from config_class default values that are known to work well with boolean logic.
-    You can define user-specific parameters in the construction of the object in __init__() or define 
-    methods that you might need after, e.g. a new fitness function or input and output generators. 
+    You can define user-specific parameters in the construction of the object in __init__() or define
+    methods that you might need after, e.g. a new fitness function or input and output generators.
     Remember if you define a new fitness function or generator, you have to redefine the self.Fitness,
     self.Target_gen and self.Input_gen in __init__()
     '''
-    
+
     def __init__(self):
         super().__init__() #DO NOT REMOVE!
 
@@ -22,23 +21,22 @@ class experiment_config(config_class):
         ################################################
         ######### USER-SPECIFIC PARAMETERS #############
         ################################################
-        
+
         ################# Save settings ################
         self.filepath = r'D:\data\BramdW\test'
         self.name = 'AND'
-        
+
         ############## New Fitness function ############
 
         ################################################
         ################# OFF-LIMITS ###################
         ################################################
         self.genomes = sum(self.partition)  # Make sure genomes parameter is correct
-    
-    #####################################################    
+
+    #####################################################
     ############# USER-SPECIFIC METHODS #################
     #####################################################
 
 
 
         pass
-    
--- modules/benchmarks/ManifoldTest.py
+++
@@ -1,9 +0,0 @@
-import Manifold
-import matplotlib.pyplot as plt
-t, x= Manifold.TargetSignal(1000)
-# t, x_or = BooleanLogic.NOR(1000)
-
-# plt.plot(t, x)
-# plt.plot(t, y)
-plt.plot(t, x)
-plt.show()
