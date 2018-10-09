b80395fa277cad8e60be6629efce0d7e5e59d560
heliox <>
2018-10-09 15:30:14 +0200

    .

--- experiments/NoiseSamplingST/config_NoiseSamplingST.py
+++ experiments/NoiseSamplingST/config_NoiseSamplingST.py
@@ -17,11 +17,11 @@ class experiment_config():
         self.fs = 1000          # Sampling freq (Hz)
         self.sampleTime = 2     # Sampling time (s)
         self.amplification = 1E9
-        self.steps = [[400, 600],[400, 600],[400, 600],[400],[400],[400],[400]]  # Steps per control voltage
+        self.steps = [[600],[0],[0],[0],[0],[0],[0]]  # Steps per control voltage
         self.gridHeight = len(self.steps[0])        # Amount of steps for a CV
         self.controls = 7
         self.T_test = True      # Tests variations in the variance for a sample time
-        self.S_test = True      # Tests variations in the variance for measure - switch - measure for one CV
+        self.S_test = False      # Tests variations in the variance for measure - switch - measure for one CV
         self.samples = 10       # Amount of measurements for one CV config
         
         self.CVsteps = [len(self.steps[i]) for i in range(len(self.steps))]
