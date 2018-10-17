a64aec5c5c981e9ea0f6e7b31b9f28f8bce2505f
heliox <>
2018-10-17 09:44:19 +0200

    Added NiDAQ IO longer time sampling

--- experiments/NoiseSamplingST/config_NoiseSamplingST.py
+++ experiments/NoiseSamplingST/config_NoiseSamplingST.py
@@ -16,7 +16,7 @@ class experiment_config(config_class):
     def __init__(self):
         super().__init__()
         self.fs = 1000          # Sampling freq (Hz)
-        self.sampleTime = 8     # Sampling time (s)
+        self.sampleTime = 1000     # Sampling time (s)
         self.res = 1E9
         self.steps = [[-623.79],[247.38],[826.70],[-294.96],[-108.88],[668.49],[-563.18]]  # Steps per control voltage
         self.controls = 7
@@ -25,7 +25,7 @@ class experiment_config(config_class):
         self.gridSearch = False     # If true: use a grid for sampling (use sekf.steps)
         self.T_test = True      # Tests variations in the variance for a sample time
         self.S_test = False      # Tests variations in the variance for measure - switch - measure for one CV
-        self.samples = 10       # Amount of measurements for one CV config
+        self.samples = 1       # Amount of measurements for one CV config
         
         self.filepath = 'D:\\data\\Mark\\ST_tests\\'
         self.name_T = 'SampleTimeMeas' + str(self.sampleTime) +'s'
@@ -34,11 +34,12 @@ class experiment_config(config_class):
         self.electrodeSetup = [[1,2,3,4,5,6,7,'grnd A'],[1,3,5,7,11,13,15,17],[5,6,7,8,1,2,3,4]]
         
         # IF CVs are already found, use this:
-        self.CVs = np.array([[-447.703,-693.694,840.182,678.188,353.957,835.205,-783.944],
-                    [-399.718,-292.205,-86.1642,900,669.22,442.349,-791.655],
-                    [-726.231,69.3323,-307.257,-233.03,-45.9899,-778.111,551.091],
-                    [282.338,-406.737,-379.836,189.312,-380.605,-333.847,716.212],
-                    [-859.277,-819.744,-344.16,818.718,442.094,-884.256,629.337]])
+        self.CVs = np.array([[-421.51,-293.851,818.862,-285.016,-22.1971,704.963,-739.663],
+[268.596,-78.589,-613.81,35.3767,-367.439,810.921,-629.311],
+[-681.055,-53.0367,48.3326,40.1132,-663.618,812.194,-619.485],
+[550.2,-817.53,640.832,-659.01,-581.936,405.044,579.165],
+[497.179,687.261,-613.696,-233.437,-322.216,-317.731,-467.174],
+[647.055,743.272,427.713,-554.105,484.863,112.871,-209.393]])
                     
     
     
@@ -50,10 +51,10 @@ class experiment_config(config_class):
         self.amplification = 1 
         self.genes = 8              # Must be 8 because boolean_logic defines control voltages for genes - 1
         self.genomes = 25
-        self.generations = 10
+        self.generations = 15
         self.generange = [[-900,900], [-900, 900], [-900, 900], [-900, 900], [-900, 900], [-900, 900], [-900, 900],[0., 1.]]
 
-        self.targetCurrent = [0.50, 1.00, 1.50, 2.00, 2.50]    # The desired output current
+        self.targetCurrent = [0.25, 0.50, 0.75, 1.00, 1.25, 1.50, 1.75, 2.00, 2.25, 2.50, 2.75, 3.00]    # The desired output current
         self.TargetGen = self.Target
         self.Fitness = self.FitnessNMSE
         self.fitThres = 1000            #Threshold for high enough fitness value during search
