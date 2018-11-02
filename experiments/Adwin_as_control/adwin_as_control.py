6352006a39783ba695f960bc71bc76172374a241
Bvdv <37548649+bvdv@users.noreply.github.com>
2018-11-02 11:21:37 +0100

    add adwina as control

---
+++ experiments/Adwin_as_control/adwin_as_control.py
@@ -0,0 +1,18 @@
+# SkyNEt imports
+import SkyNEt.modules.SaveLib as SaveLib
+from SkyNEt.instruments.ADwin import adwinIO
+import SkyNEt.modules.Evolution as Evolution
+from SkyNEt.instruments.DAC import IVVIrack
+import SkyNEt.modules.PlotBuilder as PlotBuilder
+
+# Other imports
+import time
+import numpy as np
+import signal
+import sys
+
+controlvoltages = [1, 0.5, 0.2, 0.6, 0.7, 0.8, 0.3, 0.4]
+
+adw = adwinIO.initInstrument()
+
+adwinIO.setControlVoltages(adw, controlvoltages, 1000)
