ffe3c0452bbe84fdc4c0788b754fd40d1e7fc7c3
Bvdv <37548649+bvdv@users.noreply.github.com>
2018-11-13 12:29:22 +0100

    Update NPZanalysis.py

--- NPZanalysis.py
+++ NPZanalysis.py
@@ -1,8 +1,17 @@
 import numpy as np 
+import matplotlib.pyplot as plt
+import math
 
-
-data=np.load('\Users\VenB\Desktop\\nparrays.npz')
+data=np.load('C:\\Users\\VenB\\Desktop\\Evolution\\2018_11_11_144633_AND_77%_high\\nparrays.npz')
 
 print(data.keys())
 
-data = data['data']
+farray = data['fitnessArray']
+garray = data['geneArray']
+outputArray = data['outputArray']
+outp = data['outp']
+t = data['t']
+
+for i in range(25):
+    plt.figure()
+    plt.plot(t,outputArray[0,i])
