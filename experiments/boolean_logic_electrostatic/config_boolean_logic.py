ca75e93818785785aeccd58a40938a0a0989d60e
Bram de Wilde <brambo5@live.nl>
2019-01-11 10:39:44 +0100

    Preparation for testing electrostatic controls

---
+++ experiments/boolean_logic_electrostatic/config_boolean_logic.py
@@ -0,0 +1,110 @@
+import numpy as np
+from SkyNEt.config.config_class import config_class
+
+class experiment_config(config_class):
+    '''This is the experiment configuration file for the boolean logic experiment.
+    It also serves as a template for other experiments, so please model your work
+    after this when you make new experiments.
+    This experiment_config class inherits from config_class default values that are known to work well with boolean logic.
+    You can define user-specific parameters in the construction of the object in __init__() or define
+    methods that you might need after, e.g. a new fitness function or input and output generators.
+    Remember if you define a new fitness function or generator, you have to redefine the self.Fitness,
+    self.Target_gen and self.Input_gen in __init__()
+
+    ----------------------------------------------------------------------------
+    Description of general parameters
+    ----------------------------------------------------------------------------
+    comport; the COM port to which the ivvi rack is connected.
+    amplification; specify the amount of nA/V. E.g. if you set the IVVI to 100M,
+        then amplification = 10
+    generations; the amount of generations for the GA
+    generange; the range that each gene ([0, 1]) is mapped to. E.g. in the Boolean
+        experiment the genes for the control voltages are mapped to the desired
+        control voltage range.
+    partition; this tells the GA how it will evolve the next generation.
+        In order, this will make the GA evolve the specified number with
+        - promoting fittest partition[0] genomes unchanged
+        - adding Gaussian noise to the fittest partition[1] genomes
+        - crossover between fittest partition[2] genomes
+        - crossover between fittest partition[3] and randomly selected genes
+        - randomly adding parition[4] genomes
+    genomes; the amount of genomes in the genepool, speficy this parameter instead
+        of partition if you don't care about the specific partition.
+    genes; the amount of genes per genome
+    mutationrate; the probability of mutation for each gene (between 0 and 1)
+    fitnessavg; the amount of times the same genome is tested to obtain the fitness
+        value.
+    fitnessparameters; the parameters for FitnessEvolution (see its doc for
+        specifics)
+    filepath; the path used for saving your experiment data
+    name; name used for experiment data file (date/time will be appended)
+
+    ----------------------------------------------------------------------------
+    Description of method parameters
+    ----------------------------------------------------------------------------
+    signallength; the length in s of the Boolean P and Q signals
+    edgelength; the length in s of the edge between 0 and 1 in P and Q
+    fs; sample frequency for niDAQ or ADwin
+
+    ----------------------------------------------------------------------------
+    Description of methods
+    ----------------------------------------------------------------------------
+    TargetGen; specify the target function you wish to evolve, options are:
+        - OR
+        - AND
+        - NOR
+        - NAND
+        - XOR
+        - XNOR
+    Fitness; specify the fitness function, standard options are:
+        - FitnessEvolution; standard fitness used for boolean logic
+        - FitnessNMSE; normalised mean squared error
+    '''
+
+    def __init__(self):
+        super().__init__() #DO NOT REMOVE!
+        ################################################
+        ######### SPECIFY PARAMETERS ###################
+        ################################################
+        self.comport = 'COM3'  # COM port for the ivvi rack
+        self.device = 'nidaq'  # Either nidaq or adwin
+
+        # Define experiment
+        self.amplification = 1
+        self.TargetGen = self.NOR
+        self.generations = 2
+        self.generange = [[-600,600], [-900, 900], [-900, 900], [-900, 900], [-600, 600], [0.1, 0.5]]
+
+        # Specify either partition or genomes
+        #self.partition = [5, 5, 5, 5, 5]
+        self.genomes = 10
+
+        # Documentation
+        self.genelabels = ['CV1/T11','CV2/T13','CV3/T17','CV4/T7','CV5/T1', 'Input scaling']
+
+        # Save settings
+        self.filepath = r'D:\Data\Bram\evolution_test\\'  #Important: end path with double backslash
+        self.name = 'NOR'
+
+        ################################################
+        ################# OFF-LIMITS ###################
+        ################################################
+        # Check if genomes parameter has been changed
+        if(self.genomes != sum(self.default_partition)):
+            if(self.genomes%5 == 0):
+                self.partition = [int(self.genomes/5)]*5  # Construct equally partitioned genomes
+            else:
+                print('WARNING: The specified number of genomes is not divisible by 5.'
+                      + ' The remaining genomes are generated randomly each generation. '
+                      + ' Specify partition in the config instead of genomes if you do not want this.')
+                self.partition = [self.genomes//5]*5  # Construct equally partitioned genomes
+                self.partition[-1] += self.genomes%5  # Add remainder to last entry of partition
+
+        self.genomes = int(sum(self.partition))  # Make sure genomes parameter is correct
+        self.genes = int(len(self.generange))  # Make sure genes parameter is correct
+
+    #####################################################
+    ############# USER-SPECIFIC METHODS #################
+    #####################################################
+    # Optionally define new methods here that you wish to use in your experiment.
+    # These can be e.g. new fitness functions or input/output generators.
