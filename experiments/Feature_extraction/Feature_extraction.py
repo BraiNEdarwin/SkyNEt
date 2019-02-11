0aeaa0e009728926432dacc77b351f03de853fde
Bvdv <37548649+bvdv@users.noreply.github.com>
2019-02-11 15:36:02 +0100

    feature extraction
    updated the feature extraction code to the new format.

---
+++ experiments/Feature_extraction/Feature_extraction.py
@@ -0,0 +1,147 @@
+'''
+In this script the controlvoltages corresponding to different genomes are set and a boulean logic measurement is performed.
+This is done withing 3 for loops:
+1. for i in range generations for every generation rankes the fitnsessed of the genomes (control voltage sets) of the generation and used the GA to define the genomes of the next generation.
+   After this the output with the best fitness as well as this fitness compared to the generation are plotted.
+2. for j in range genomes for ever genome in a generation it sets the desisred control voltages on the DAC's of the IVVI-rack and scales the boulean logic input.
+   Next, it uses either the adwin or nidaq to measure the boulean logic input output relation after which the coresponding fitness is calculated.
+   Finally, the genomes and output of each genome is plotted.
+3. for k in range genes is use to map the 0-1 generated genome to the generange at put it in the desired orded in the coltrolvoltage array. 
+
+
+'''
+
+# SkyNEt imports
+import SkyNEt.modules.SaveLib as SaveLib
+import SkyNEt.modules.Evolution as Evolution
+import SkyNEt.modules.PlotBuilder as PlotBuilder
+import config_Feature_extract as config
+from SkyNEt.instruments import InstrumentImporter
+
+# Other imports
+import time
+import numpy as np
+
+# Initialize config object
+cf = config.experiment_config()
+
+# Initialize input and target
+inp = cf.InputGen()
+meas = np.zeros(cf.measurelength)
+# np arrays to save genePools, outputs and fitness
+geneArray = np.zeros((cf.generations, cf.genomes, cf.genes))
+outputArray = np.zeros((cf.generations, cf.genomes, len(inp), cf.ntest))
+fitnessArray = np.zeros((cf.generations, cf.genomes))
+
+# Temporary arrays, overwritten each generation
+fitnessTemp = np.zeros((cf.genomes, cf.fitnessavg))
+outputAvg = np.zeros((cf.fitnessavg, len(x[0])))
+outputTemp = np.zeros((cf.genomes, len(x[0])))
+controlVoltages = np.zeros(7)
+
+# Initialize save directory
+saveDirectory = SaveLib.createSaveDirectory(cf.filepath, cf.name)
+
+# Initialize main figure
+mainFig = PlotBuilder.initMainFigEvolution(cf.genes, cf.generations, cf.genelabels, cf.generange)
+
+# Initialize instruments
+ivvi = InstrumentImporter.IVVIrack.initInstrument()
+
+# Initialize genepool
+genePool = Evolution.GenePool(cf)
+
+#%% Measurement loop
+
+for i in range(cf.generations):
+    for j in range(cf.genomes):
+        # Set the DAC voltages
+        contorls = np.zeros(cf.genes)
+        for k in range(cf.genes-1):
+            controls[k] = genePool.MapGenes(
+                                    cf.generange[k], genePool.pool[j, k])
+        controlVoltages[4:] = controls[:3]
+        InstrumentImporter.IVVIrack.setControlVoltages(ivvi, controlVoltages)
+        print("current CV1,CV2,CV3")
+        print(controlVoltages[4:])
+
+        # Set the input scaling
+        x_scaled = x * genePool.MapGenes(cf.generange[-1], genePool.pool[j, -1])
+
+        # Measure cf.fitnessavg times the current configuration
+        for avgIndex in range(cf.fitnessavg):
+            # set inputs to dacs
+            for n in range(len(inp)):
+              inputVoltages = [(inp[n,0])*1000-500, (inp[n,1])*1000-500, (winp[n,2])*1000-500, (inp[n,3])*1000-500]
+              print("current input")
+              print(inputVoltages)
+              InstrumentImporter.IVVIrack.setControlVoltages(ivvi, controlVoltages)
+              time.sleep(0.2)
+              for m in range(cf.ntest):
+            # Feed input to measurement device
+                if(cf.device == 'nidaq'):
+                    measureddata = np.asarray(InstrumentImporter.nidaqIO.IO(meas, cf.fs))*cf.amplification
+                elif(cf.device == 'adwin'):
+                    adw = InstrumentImporter.adwinIO.initInstrument()
+                    output = InstrumentImporter.adwinIO.IO(adw, x_scaled, cf.fs)
+                else:
+                    print('Specify measurement device as either adwin or nidaq')
+                output[n,m] = np.average(measureddata)
+
+            # Plot genome
+            PlotBuilder.currentGenomeEvolution(mainFig, genePool.pool[j])
+
+            # Train output
+            outputAvg[avgIndex] = cf.amplification * np.asarray(output)  # empty for now, as we have only one output node
+
+            # Calculate fitness
+            fitnessTemp[j, avgIndex]= cf.Fitness(output,
+                                                     cf.TargetGen,)
+            # Plot output
+            PlotBuilder.currentOutputClassification(mainFig,
+                                               output,
+                                               inp,
+                                               j + 1, i + 1,
+                                               fitnessTemp[j, avgIndex],
+                                               cf.TargetGen)
+
+        outputTemp[j] = outputAvg[np.argmin(fitnessTemp[j])]
+
+    genePool.fitness = fitnessTemp.min(1)  # Save fitness
+
+    # Status print
+    print("Generation nr. " + str(i + 1) + " completed")
+    print("Highest fitness: " + str(max(genePool.fitness)))
+
+    # Save generation data
+    geneArray[i, :, :] = genePool.pool
+    outputArray[i, :, :] = outputTemp
+    fitnessArray[i, :] = genePool.fitness
+
+    # Update main figure
+    PlotBuilder.updateMainFigEvolution(mainFig,
+                                       geneArray,
+                                       fitnessArray,
+                                       outputArray,
+                                       i + 1,
+                                       t,
+                                       cf.amplification*target,
+                                       output,
+                                       w)
+
+    # Save generation
+    SaveLib.saveExperiment(saveDirectory,
+                     geneArray = geneArray,
+                     outputArray = outputArray,
+                     fitnessArray = fitnessArray,
+                     input = inp,
+                     target = cf.TargetGen,
+                     amplified_target = cf.amplification*target)
+
+    # Evolve to the next generation
+    genePool.NextGen()
+
+PlotBuilder.finalMain(mainFig)
+
+InstrumentImporter.reset(0, 0)
+
---
+++ experiments/Feature_extraction/config_Feature_extraction.py
@@ -0,0 +1,159 @@
+import numpy as np
+import itertools
+from SkyNEt.config.config_class import config_class
+
+class experiment_config(config_class):
+    '''This is the experiment configuration file for the boolean logic experiment.
+    It also serves as a template for other experiments, so please model your work
+    after this when you make new experiments.
+    This experiment_config class inherits from config_class default values that are known to work well with boolean logic.
+    You can define user-specific parameters in the construction of the object in __init__() or define
+    methods that you might need after, e.g. a new fitness function or input and output generators.
+    Remember if you define a new fitness function or generator, you have to redefine the self.Fitness,
+    self.Target_gen and self.Input_gen in __init__()
+
+    ----------------------------------------------------------------------------
+    Description of general parameters
+    ----------------------------------------------------------------------------
+    comport; the COM port to which the ivvi rack is connected.
+    amplification; specify the amount of nA/V. E.g. if you set the IVVI to 100M,
+        then amplification = 10
+    generations; the amount of generations for the GA
+    generange; the range that each gene ([0, 1]) is mapped to. E.g. in the Boolean
+        experiment the genes for the control voltages are mapped to the desired
+        control voltage range.
+    partition; this tells the GA how it will evolve the next generation.
+        In order, this will make the GA evolve the specified number with
+        - promoting fittest partition[0] genomes unchanged
+        - adding Gaussian noise to the fittest partition[1] genomes
+        - crossover between fittest partition[2] genomes
+        - crossover between fittest partition[3] and randomly selected genes
+        - randomly adding parition[4] genomes
+    genomes; the amount of genomes in the genepool, speficy this parameter instead
+        of partition if you don't care about the specific partition.
+    genes; the amount of genes per genome
+    mutationrate; the probability of mutation for each gene (between 0 and 1)
+    fitnessavg; the amount of times the same genome is tested to obtain the fitness
+        value.
+    fitnessparameters; the parameters for FitnessEvolution (see its doc for
+        specifics)
+    filepath; the path used for saving your experiment data
+    name; name used for experiment data file (date/time will be appended)
+
+    ----------------------------------------------------------------------------
+    Description of method parameters
+    ----------------------------------------------------------------------------
+    signallength; the length in s of the Boolean P and Q signals
+    edgelength; the length in s of the edge between 0 and 1 in P and Q
+    fs; sample frequency for niDAQ or ADwin
+
+    ----------------------------------------------------------------------------
+    Description of methods
+    ----------------------------------------------------------------------------
+    TargetGen; specify the target function you wish to evolve, options are:
+        - OR
+        - AND
+        - NOR
+        - NAND
+        - XOR
+        - XNOR
+    Fitness; specify the fitness function, standard options are:
+        - FitnessEvolution; standard fitness used for boolean logic
+        - FitnessNMSE; normalised mean squared error
+    '''
+
+    def __init__(self):
+        super().__init__() #DO NOT REMOVE!
+        ################################################
+        ######### SPECIFY PARAMETERS ###################
+        ################################################
+        self.comport = 'COM3'  # COM port for the ivvi rack
+        self.device = 'nidaq'  # Either nidaq or adwin
+
+        # Define experiment
+        self.amplification = 10
+        self.TargetGen = 0 # defines which feature needs to be extraction from 0-15
+        self.generations = 100
+        self.generange = [[-1500,1500], [-1500, 1500], [-1500, 1500]]
+
+        # Specify either partition or genomes
+        #self.partition = [5, 5, 5, 5, 5]
+        self.genomes = 25
+        self.ntest = 10
+        self.measurelength = 200
+
+        # Documentation
+        self.genelabels = ['CV1','CV2','CV3']
+
+        # Save settings
+        self.filepath = r'D:\Data\Bram\Feature_extracion\\'  #Important: end path with double backslash
+        self.name = '0000'
+
+        self.Fitness = self.Fitness_extractiondiff
+        self.InputGen = self.Fe_input
+
+        ################################################
+        ################# OFF-LIMITS ###################
+        ################################################
+        # Check if genomes parameter has been changed
+        if(self.genomes != sum(self.default_partition)):
+            if(self.genomes%5 == 0):
+                self.partition = [int(self.genomes/5)]*5  # Construct equally partitioned genomes
+            else:
+                print('WARNING: The specified number of genomes is not divisible by 5.'
+                      + ' The remaining genomes are generated randomly each generation. '
+                      + ' Specify partition in the config instead of genomes if you do not want this.')
+                self.partition = [self.genomes//5]*5  # Construct equally partitioned genomes
+                self.partition[-1] += self.genomes%5  # Add remainder to last entry of partition
+
+        self.genomes = int(sum(self.partition))  # Make sure genomes parameter is correct
+        self.genes = int(len(self.generange))  # Make sure genes parameter is correct
+
+    #####################################################
+    ############# USER-SPECIFIC METHODS #################
+    #####################################################
+    # Optionally define new methods here that you wish to use in your experiment.
+    # These can be e.g. new fitness functions or input/output generators.
+
+    def Fitness_extractiondiff(self, output, marker):
+    	I_std = np.zeros([16])
+        I_average = np.zeros([16])
+
+        #find the average current and standard deviation of all outputs and without the desired feature. 
+        for i in range(16):
+            I_std[i] = np.std(output[i])
+            I_average[i] = np.average(output[i])
+        I_other = np.delete(I_average, marker)
+        I_otherstd = np.delete(I_std, marker)
+
+        indexh = np.argmax(I_other+I_otherstd)
+        indexl = np.argmin(I_other-I_otherstd)
+
+        #distinguis if the feature will have to go for a positive or negative extractor and check how far it is away from this feature. 
+        #done for higher but negative and lower but positive ass well.
+        if (I_average[marker]+I_std[marker])-(I_other[indexh]-I_otherstd[indexh])<(I_average[marker]-I_std[marker])-(I_other[indexl]+I_otherstd[indexl]):
+            F = I_average[marker]+I_std[marker]-I_other[indexh]-I_otherstd[indexh]
+        else 
+            F = I_average[marker]-I_std[marker]-I_other[indexl]+I_otherstd[indexl]
+
+
+        #Fitness for when positive needs to be highest and negative needs to be lowest.
+        # if I_average[marker] > 0 
+        #     F = I_average[marker]+I_std[marker]-I_other[indexh]-I_otherstd[indexh]
+        # else 
+        #     F = I_average[marker]-I_std[marker]-I_other[indexl]+I_otherstd[indexl]
+
+
+        for i in range(len(output)): 
+            for j in range(len(output[0])):
+                if(abs(output[i][j])>3.1*10):
+                    return -100
+
+        return F
+
+
+
+    def Fe_input(self):
+        a = [0, 1]
+        inp = np.array(list(itertools.product(*[a,a,a,a])))
+        return inp
