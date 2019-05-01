10cc8593794ab0907586ec532900afd59a183538
RikFreije <49909357+RikFreije@users.noreply.github.com>
2019-05-01 16:43:47 +0200

    Update adwinIO.py

--- instruments/ADwin/adwinIO.py
+++ instruments/ADwin/adwinIO.py
@@ -73,7 +73,7 @@ def IO(adw, Input, Fs, inputPorts = [1, 0, 0, 0, 0, 0, 0]):
     # Input preparation
     if len(Input.shape) == 1:
         Input = Input[np.newaxis,:]
-#watch out for rikbeuncode, this code might only run for specific inputs
+
     inputs = Input.copy()
     InputSize = inputs.shape[1]
     x = np.zeros((8, InputSize), dtype = int)
