c34570b7464c622faeee0abdbef668b4af78b3aa
internetvandingen <internetvandingen@protonmail.com>
2019-05-12 01:12:48 +0200

    legend support, update subplot by name, converted boolean experiment, only maximizes on Qt5 backend

--- modules/PlotBuilder.py
+++ modules/PlotBuilder.py
@@ -6,65 +6,79 @@ Created on Tue May  7 13:52:06 2019
 @author: ljknoll
 
 TODO:
-    add legend option
     write documentation
+    set only lower or upper limit
 
 
 Example:
-    import time
-    import numpy as np
-    pb = PB()
-    pb.add_subplot((0,0), (2,m),  ylim=(0,1), rowspan=2, xlabel='testx', title='testtitle', adaptive=True)
-    pb.add_subplot((0,1), m, ylim=(0,1))
-    pb.add_subplot((1,1), n, adaptive=True)    
-    pb.finalize()
-    
-    t_start = time.time()
-    for i in range(1, n):
-        genome3 = np.random.rand(m)
-        pb.update((0,1), genome3)
-        if i%10==0:
-            genomes = np.random.rand(2,i) + np.arange(i)/5
-            pb.update((0,0), genomes)
-            genome = np.random.rand(i)+np.arange(i)
-            pb.update((1,1), genome)
-    avg = (time.time() - t_start)/n
-    print('Average time drawing one frame: %0.4f' %avg)
+
+import time
+import numpy as np
+m,n = 10,100
+pb = PlotBuilder()
+pb.add_subplot((0,0), (2,m),  ylim=(0,1), rowspan=2, xlabel='testx', title='testtitle', adaptive=True)
+pb.add_subplot((0,1), m, ylim=(0,1))
+pb.add_subplot((1,1), n, adaptive=True)    
+pb.finalize()
+
+t_start = time.time()
+for i in range(1, n):
+    genome3 = np.random.rand(m)
+    pb.update((0,1), genome3)
+    if i%10==0:
+        genomes = np.random.rand(2,i) + np.arange(i)/5
+        pb.update((0,0), genomes)
+        genome = np.random.rand(i)+np.arange(i)
+        pb.update((1,1), genome)
+avg = (time.time() - t_start)/n
+print('Average time drawing one frame: %0.4f' %avg)
 
 
 """
 
 import numpy as np
+import time
 import matplotlib
 import matplotlib.pyplot as plt
 
 class PlotBuilder:
     def __init__(self):
-        assert matplotlib.get_backend() == 'Qt5Agg', "Error: In order to maximize figure, we need Qt5Agg backend. Try typing '%matplotlib' in ipython console"
+        if matplotlib.get_backend() == 'Qt5Agg':
+            self.maximize = True
+        else:
+            self.maximize = False
+            print("\x1b[33mWARN: In order to maximize figure, we need Qt backend. Try typing '%matplotlib qt' in your ipython console.\x1b[0m")
         self.plots = {}
         self.data_size = {}
         self.adaptive = {}
+        self.legends = {}
+        self.names = {}
     
-    def add_subplot(self, pos, data_size, adaptive=False, **kwargs):
+    def add_subplot(self, name, pos, data_size, adaptive=False, legend=None, **kwargs):
         """ 
+        name        string: name which can be used to update the figure
         pos         tuple:  (y, x) position on figure grid
         data_size   int:    length of ydata in plot (ignored if adaptive)
                     tuple:  (nr_lines, n) plot number of lines each of size n
         adaptive    bool:   Whether axes sizes are adaptive to new data,
                              can be used together with ylim, but xlim argument is ignored.
+        legend      list:   List of strings to be put in the legend
         
         Warning: plotting takes some time (~50ms), if you want to speed things up, plot less often
         
         extra keyword arguments are used to set axis properties.
         e.g. if there is a keyword argument title = 'test title',
         then axes.set_title('test title') will be called on the subplot.
-        See https://matplotlib.org/2.0.2/api/axes_api.html and search for set_ 
+        
+        See https://matplotlib.org/2.0.2/api/axes_api.html and search 'set_' for all available properties
         """
-        self.data_size[pos] = data_size
-        self.adaptive[pos] = adaptive
         if adaptive:
             kwargs.pop('xlim', None)
+        self.data_size[pos] = data_size
+        self.adaptive[pos] = adaptive
+        self.legends[pos] = legend
         self.plots[pos] = kwargs
+        self.names[name] = pos
 
     
     def finalize(self):
@@ -87,10 +101,11 @@ class PlotBuilder:
                     rowspan=pltargs.pop('rowspan', 1))
             self.axes[pos].set(**pltargs)
         
-        # maximize figure
-        figManager = self.fig.canvas.manager
-        figManager.window.showMaximized()
-        
+        if self.maximize:
+            # maximize figure, works only on Qt backends
+            figManager = self.fig.canvas.manager
+            figManager.window.showMaximized()
+
         # It is necessary to draw the canvas at least once before draw_artist can be called
         self.fig.canvas.draw()
         
@@ -103,14 +118,18 @@ class PlotBuilder:
             dummydata = np.zeros(size)
             # create and store lines such that their data can be overwritten later
             self.lines[pos] = ax.plot(np.arange(dummydata.shape[-1]), dummydata.T)
+            if self.legends[pos] is not None:
+                ax.legend(self.lines[pos], self.legends[pos], loc='upper left')
             self.backgrounds[pos] = self.fig.canvas.copy_from_bbox(ax.bbox)
+
         # use tight layout
         self.fig.tight_layout()
-    
-    def update(self, subplot, ydata):
+
+    def update(self, name, ydata):
+        subplot = self.names[name]
         self.fig.canvas.restore_region(self.backgrounds[subplot])
         nr_points = ydata.shape[-1]
-        ydata = ydata.reshape(-1,nr_points) # reshape to (nr_lines, nr_points) even for one dimension
+        ydata = ydata.reshape(-1,nr_points) # reshape to (nr_lines, nr_points), also for a single line
         for i,line in enumerate(self.lines[subplot]):
             line.set_ydata(ydata[i])
             line.set_xdata(np.arange(nr_points))
