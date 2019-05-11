2d0b12445d0c5f9d146bb4ca4bd057cc5777c357
internetvandingen <internetvandingen@protonmail.com>
2019-05-12 01:38:43 +0200

    update example

--- modules/PlotBuilder.py
+++ modules/PlotBuilder.py
@@ -8,6 +8,7 @@ Created on Tue May  7 13:52:06 2019
 TODO:
     write documentation
     set only lower or upper limit
+    tight_layout doesn't seem to work
 
 
 Example:
@@ -16,20 +17,21 @@ import time
 import numpy as np
 m,n = 10,100
 pb = PlotBuilder()
-pb.add_subplot((0,0), (2,m),  ylim=(0,1), rowspan=2, xlabel='testx', title='testtitle', adaptive=True)
-pb.add_subplot((0,1), m, ylim=(0,1))
-pb.add_subplot((1,1), n, adaptive=True)    
+pb.add_subplot('big_plot', (0,0), (2,0),  ylim=(0,1), adaptive=True, rowspan=2, title='adaptive X, static Y', xlabel='generations')
+pb.add_subplot('static', (0,1), m, ylim=(0,1), title='static axis are quicker', xlabel='genes')
+pb.add_subplot('adaptive', (1,1), n, adaptive=True, title='both axes are adaptive', xlabel='iteration')
 pb.finalize()
 
 t_start = time.time()
+genomes = np.zeros((2,n))
 for i in range(1, n):
     genome3 = np.random.rand(m)
-    pb.update((0,1), genome3)
+    pb.update('static', genome3)
     if i%10==0:
-        genomes = np.random.rand(2,i) + np.arange(i)/5
-        pb.update((0,0), genomes)
-        genome = np.random.rand(i)+np.arange(i)
-        pb.update((1,1), genome)
+        genomes[:,i//10] = np.random.rand(2)
+        pb.update('big_plot', genomes[:, :i//10+1])
+        genome = np.random.rand(i)*n/m+np.arange(i)
+        pb.update('adaptive', genome)
 avg = (time.time() - t_start)/n
 print('Average time drawing one frame: %0.4f' %avg)
 
